844c515143b171d54ca3fcc4f8edfd2bc25fc56e
=Goza de Farinha <odamasenganumeko@gmail.com>
2023-11-16 21:20:28 -0300

    Adicionada model composta de Compra, ItensProd e Produtos

---
+++ PAKTD/Models/MO/vmCompraItensProd.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PAKTD.Models.MO
+{
+    public class vmCompraItensProd
+    {
+
+        public mProduto mP { get; set; }
+        public mCompra mC { get; set; }
+        public mCompra Carrinho { get; set; }
+    }
+}
