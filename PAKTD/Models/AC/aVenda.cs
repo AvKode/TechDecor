a2892cf949511410793371b61af9c06c37dc9e44
=Goza de Farinha <odamasenganumeko@gmail.com>
2023-11-23 20:49:31 -0300

    Adicionada ações de Venda

---
+++ PAKTD/Models/AC/aVenda.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using PAKTD.Models.MO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PAKTD.Models.AC
+{
+    public class aVenda
+    {
+
+        conexao con = new conexao();
+
+        public void InserirVenda(mCompra mV)
+        {
+            MySqlCommand cmd = new MySqlCommand("call sp_InserirCliUsu(@nmUsu, @seUsu, @nmCli, @emCli, @cpfCli, @dtCli, @noCasa, @coCasa, @cepCli, @foCli )", con.MyConectarBD());
+
+            cmd.Parameters.AddWithValue("@dataCompra", mV.DtCompra);
+            cmd.Parameters.AddWithValue("@prazoServico", mV.DtReuniao);
+            cmd.Parameters.AddWithValue("@idCliente", mV.IdfkCli);
+
+            cmd.ExecuteNonQuery();
+            con.MyDesConectarBD();
+        }
+
+    }
+}
